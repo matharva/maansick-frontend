ca5fed2fa741d8f9ca45fd12f755c16438d4134e
matharva <atharvamohite20@gmail.com>
2022-08-24 22:22:22 +0530

    updated workflow

--- src/components/MRIStoryBoard.js
+++ src/components/MRIStoryBoard.js
@@ -1,24 +1,66 @@
-import React from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
 import { EffectCards } from "swiper";
 
-import 'swiper/css';
+import "swiper/css";
 import "swiper/css/effect-cards";
+
+const renderSliderCards = () => {
+  const items = [
+    {
+      name: "slider1",
+      color: "bg-red-100",
+    },
+    {
+      name: "slider2",
+      color: "bg-red-200",
+    },
+    {
+      name: "slider3",
+      color: "bg-red-300",
+    },
+  ];
+  return items.map((item) => {
+    return (
+      <>
+        <SwiperSlide
+          className={`flex items-center justify-center ${item.color} rounded-md`}
+        >
+          {item.name}
+        </SwiperSlide>
+      </>
+    );
+  });
+};
+
 const MRIStoryBoard = () => {
-    return ( <div className='flex-row z-10 h-max'>
+  return (
+    <div className="flex-row h-max">
       <Swiper
         effect={"cards"}
         grabCursor={true}
         modules={[EffectCards]}
         className="justify-center items-center w-7/12 h-96"
       >
-        <SwiperSlide className='flex items-center justify-center bg-red-500 rounded-md'>Slide 1</SwiperSlide>
-        <SwiperSlide className='flex items-center justify-center bg-sky-500 rounded-md'>Slide 2</SwiperSlide>
-        <SwiperSlide className='flex items-center justify-center bg-violet-500 rounded-md'>Slide 3</SwiperSlide>
-        <SwiperSlide className='flex items-center justify-center bg-green-500 rounded-md'>Slide 4</SwiperSlide>
-        <SwiperSlide className='flex items-center justify-center bg-pink-500 rounded-md'>Slide 5</SwiperSlide>
+        {/* <SwiperSlide className="flex items-center justify-center bg-red-500 rounded-md">
+          Slide 1
+        </SwiperSlide>
+        <SwiperSlide className="flex items-center justify-center bg-sky-500 rounded-md">
+          Slide 2
+        </SwiperSlide>
+        <SwiperSlide className="flex items-center justify-center bg-violet-500 rounded-md">
+          Slide 3
+        </SwiperSlide>
+        <SwiperSlide className="flex items-center justify-center bg-green-500 rounded-md">
+          Slide 4
+        </SwiperSlide> */}
+        {renderSliderCards()}
+        <SwiperSlide className="flex items-center justify-center bg-pink-500 rounded-md">
+          <div className="div">Load 3D model</div>
+        </SwiperSlide>
       </Swiper>
-    </div>  );
-}
- 
+    </div>
+  );
+};
+
 export default MRIStoryBoard;
--- src/pages/MRIViewer.jsx
+++ src/pages/MRIViewer.jsx
@@ -84,7 +84,7 @@ const MRIViewer = () => {
           <div className="text-4xl font-bold text-center ">Results</div>
           <div className="text-9xl py-10 font-bold">90%</div>
           <button
-            onClick={() => navigate("/mri-stages")}
+            onClick={() => navigate("/loading")}
             class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-bold rounded-lg text-lg px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
           >
             View Detailed Results
