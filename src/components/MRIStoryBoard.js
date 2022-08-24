32dc3c2971bee2ef149c2140e5e8de316d7e2497
matharva <atharvamohite20@gmail.com>
2022-08-24 23:44:48 +0530

    loading added

--- src/components/MRIStoryBoard.js
+++ src/components/MRIStoryBoard.js
@@ -2,22 +2,30 @@ import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { EffectCards } from "swiper";
 
+import SwiperCore, { Autoplay } from "swiper";
+
 import "swiper/css";
 import "swiper/css/effect-cards";
+import IMG from "../images/fa.png";
+
+SwiperCore.use([Autoplay]);
 
 const renderSliderCards = () => {
   const items = [
     {
       name: "slider1",
       color: "bg-red-100",
+      img: IMG,
     },
     {
       name: "slider2",
       color: "bg-red-200",
+      img: IMG,
     },
     {
       name: "slider3",
       color: "bg-red-300",
+      img: IMG,
     },
   ];
   return items.map((item) => {
@@ -26,7 +34,8 @@ const renderSliderCards = () => {
         <SwiperSlide
           className={`flex items-center justify-center ${item.color} rounded-md`}
         >
-          {item.name}
+          {/* {item.name} */}
+          <img src={item.img} alt="" />
         </SwiperSlide>
       </>
     );
@@ -38,22 +47,15 @@ const MRIStoryBoard = () => {
     <div className="flex-row h-max">
       <Swiper
         effect={"cards"}
+        // autoplay={{
+        //   delay: 2000,
+        //   disableOnInteraction: false,
+        //   waitForTransition: true,
+        // }}
         grabCursor={true}
         modules={[EffectCards]}
         className="justify-center items-center w-7/12 h-96"
       >
-        {/* <SwiperSlide className="flex items-center justify-center bg-red-500 rounded-md">
-          Slide 1
-        </SwiperSlide>
-        <SwiperSlide className="flex items-center justify-center bg-sky-500 rounded-md">
-          Slide 2
-        </SwiperSlide>
-        <SwiperSlide className="flex items-center justify-center bg-violet-500 rounded-md">
-          Slide 3
-        </SwiperSlide>
-        <SwiperSlide className="flex items-center justify-center bg-green-500 rounded-md">
-          Slide 4
-        </SwiperSlide> */}
         {renderSliderCards()}
         <SwiperSlide className="flex items-center justify-center bg-pink-500 rounded-md">
           <div className="div">Load 3D model</div>
--- src/pages/Loading.jsx
+++ src/pages/Loading.jsx
@@ -1,18 +1,39 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Loading = () => {
   const navigate = useNavigate();
+
+  useEffect(() => {
+    setTimeout(() => {
+      navigate("/mri-stages");
+    }, 2000);
+  }, []);
   return (
     <>
-      <div id="app"></div>
+      {/* <div id="app"></div>
       <button
         className="mt-10"
         onClick={() => navigate("/mri-stages")}
         style={{ marginTop: "100px" }}
       >
         navigate
-      </button>
+      </button> */}
+      <div className="relative">
+        <div
+          className="absolute text-4xl font-bold text-white"
+          style={{ position: "absolute", top: "10%", right: "40%" }}
+        >
+          Loading Model...
+        </div>
+        <iframe
+          width={"100%"}
+          height={"100%"}
+          className="h-screen w-screen"
+          src="http://localhost:1234"
+          // frameborder="0"
+        ></iframe>
+      </div>
     </>
   );
 };
