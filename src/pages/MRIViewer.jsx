5ed52f45325614036b016ed980534389247e558d
matharva <atharvamohite20@gmail.com>
2022-08-25 15:40:09 +0530

    chatbot added

--- src/pages/MRIViewer.jsx
+++ src/pages/MRIViewer.jsx
@@ -59,7 +59,7 @@ const MRIViewer = ({ niiFile, SVMResult, bulkFiles }) => {
             <div className="my-4">Fetching results...</div>
           )}
           <button
-            onClick={() => navigate("/loading")}
+            // onClick={() => navigate("/loading")}
             class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-bold rounded-lg text-lg px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
           >
             View Detailed Results
